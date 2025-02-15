19b6efc9cc2facc97fddede1288aa757ecfb5a8b
zhanna-minnni <z_minnibaeva@mail.ru>
2025-02-15 15:17:46 +0400

    Update database.py

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
+
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
