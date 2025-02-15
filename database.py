77352f85209d4c8019a426a7194b01212d284bfd
zhanna-minnni <z_minnibaeva@mail.ru>
2025-02-15 17:14:41 +0400

    Update database.py

--- database.py
+++ database.py
@@ -5,9 +5,11 @@ from models import Base
 DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session: sessionmaker[AsyncSession] = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
 
 
-async def startup():
+async def startup() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
