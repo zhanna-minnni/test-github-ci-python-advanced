3613cdd977d76bf227533b2e281a041ae2041fac
zhanna-minnni <z_minnibaeva@mail.ru>
2025-02-15 20:56:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from sqlalchemy import update
 from sqlalchemy.future import select
 
 from database import async_session, engine
-from models import Base, Recipe as RecipeModel
-from schemas import Recipe as RecipeSchema, RecipeCreate
+from models import Base
+from models import Recipe as RecipeModel
+from schemas import Recipe as RecipeSchema
+from schemas import RecipeCreate
 
 app = FastAPI()
+
 
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+
 @app.on_event("shutdown")
 async def shutdown():
     await engine.dispose()
+
 
 @app.post("/recipes/", response_model=RecipeSchema)
 async def create_recipe(recipe: RecipeCreate):
@@ -28,6 +33,7 @@ async def create_recipe(recipe: RecipeCreate):
         await session.refresh(new_recipe)
         return new_recipe
 
+
 @app.get("/recipes/", response_model=List[RecipeSchema])
 async def get_recipes():
     async with async_session() as session:
@@ -37,6 +43,7 @@ async def get_recipes():
             )
         )
         return result.scalars().all()
+
 
 @app.get("/recipes/{recipe_id}", response_model=RecipeSchema)
 async def get_recipe(recipe_id: int):
@@ -55,3 +62,4 @@ async def get_recipe(recipe_id: int):
         if recipe is None:
             raise HTTPException(status_code=404, detail="Recipe not found")
         return recipe
+
