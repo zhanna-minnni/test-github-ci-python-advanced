d25d89150edcfda4ad725442f5a509cf2aa2e4c7
zhanna-minnni <z_minnibaeva@mail.ru>
2025-02-15 20:50:45 +0400

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
+from typing import Any
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
+Base: Any = declarative_base()
 
 class Recipe(Base):
     __tablename__ = "recipes"
