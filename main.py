48a9a78e032f7bb127d80e07d05b5a85d48ad65c
zhanna-minnni <z_minnibaeva@mail.ru>
2025-02-19 14:46:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,58 @@
-from typing import List
+from typing import List, Annotated
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy import update
 from sqlalchemy.future import select
 
 from database import async_session, engine
-from models import Base
-from models import Recipe as RecipeModel
-from schemas import Recipe as RecipeSchema
-from schemas import RecipeCreate
+from models import Base, Recipe as RecipeModel
+from schemas import Recipe as RecipeSchema, RecipeCreate
 
 app = FastAPI()
-
 
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-
 @app.on_event("shutdown")
 async def shutdown():
     await engine.dispose()
 
+async def get_db():
+    async with async_session() as session:
+        yield session
 
 @app.post("/recipes/", response_model=RecipeSchema)
-async def create_recipe(recipe: RecipeCreate):
-    async with async_session() as session:
-        new_recipe = RecipeModel(**recipe.model_dump())
-        session.add(new_recipe)
-        await session.commit()
-        await session.refresh(new_recipe)
-        return new_recipe
-
+async def create_recipe(recipe: RecipeCreate, db: AsyncSession = Depends(get_db)):
+    new_recipe = RecipeModel(**recipe.model_dump())
+    db.add(new_recipe)
+    await db.commit()
+    await db.refresh(new_recipe)
+    return new_recipe
 
 @app.get("/recipes/", response_model=List[RecipeSchema])
-async def get_recipes():
-    async with async_session() as session:
-        result = await session.execute(
-            select(RecipeModel).order_by(
-                RecipeModel.views.desc(), RecipeModel.cooking_time
-            )
+async def get_recipes(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(RecipeModel).order_by(
+            RecipeModel.views.desc(), RecipeModel.cooking_time
         )
-        return result.scalars().all()
-
+    )
+    return result.scalars().all()
 
 @app.get("/recipes/{recipe_id}", response_model=RecipeSchema)
-async def get_recipe(recipe_id: int):
-    async with async_session() as session:
-        await session.execute(
-            update(RecipeModel.__table__)
-            .where(RecipeModel.id == recipe_id)
-            .values(views=RecipeModel.views + 1)
-        )
-        await session.commit()
+async def get_recipe(recipe_id: int, db: AsyncSession = Depends(get_db)):
+    await db.execute(
+        update(RecipeModel.__table__)
+        .where(RecipeModel.id == recipe_id)
+        .values(views=RecipeModel.views + 1)
+    )
+    await db.commit()
 
-        result = await session.execute(
-            select(RecipeModel).where(RecipeModel.id == recipe_id)
-        )
-        recipe = result.scalars().first()
-        if recipe is None:
-            raise HTTPException(status_code=404, detail="Recipe not found")
-        return recipe
+    result = await db.execute(
+        select(RecipeModel).where(RecipeModel.id == recipe_id)
+    )
+    recipe = result.scalars().first()
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipe
