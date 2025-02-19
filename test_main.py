339e0d7f1fa5c569b03481bbbe2ca8b7d48a03e7
zhanna-minnni <z_minnibaeva@mail.ru>
2025-02-19 10:02:30 +0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,9 +4,9 @@ from main import app
 
 client = TestClient(app)
 
-@pytest.mark.skip(reason="Skipping the test")
-def test_create_recipe() -> None:
-    response = client.post(
+
+def create_test_recipe():
+    return client.post(
         "/recipes/",
         json={
             "name": "Test Recipe",
@@ -15,27 +15,23 @@ def test_create_recipe() -> None:
             "description": "Test Description",
         },
     )
+
+
+def test_create_recipe() -> None:
+    response = create_test_recipe()
     assert response.status_code == 200
     assert "id" in response.json()
 
 
-@pytest.mark.skip(reason="Skipping the test")
 def test_get_recipes() -> None:
+    create_test_recipe()
     response = client.get("/recipes/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
 
 def test_get_recipe() -> None:
-    create_response = client.post(
-        "/recipes/",
-        json={
-            "name": "Test Recipe",
-            "cooking_time": 30,
-            "ingredients": "Test Ingredients",
-            "description": "Test Description",
-        },
-    )
+    create_response = create_test_recipe()
     recipe_id = create_response.json()["id"]
 
     response = client.get(f"/recipes/{recipe_id}")
@@ -43,7 +39,6 @@ def test_get_recipe() -> None:
     assert response.json()["id"] == recipe_id
 
 
-@pytest.mark.skip(reason="Skipping the test")
 def test_get_nonexistent_recipe() -> None:
     response = client.get("/recipes/9999")
     assert response.status_code == 404
